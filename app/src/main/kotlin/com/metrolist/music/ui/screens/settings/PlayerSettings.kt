19dee2bec9bcb2b055bf84d3bcfc10dc5b26ea1c
Mo Agamy <80542861+mostafaalagamy@users.noreply.github.com>
2025-06-19 17:23:43 +0300

    Update PlayerSettings.kt

--- app/src/main/kotlin/com/metrolist/music/ui/screens/settings/PlayerSettings.kt
+++ app/src/main/kotlin/com/metrolist/music/ui/screens/settings/PlayerSettings.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.WindowInsetsSides
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.only
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.foundation.rememberScrollState
@@ -32,6 +33,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
